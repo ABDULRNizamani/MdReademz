b41811c8cae94fec2534e2135006430d1bf68ec5
Abdul Rehman <abdulrehman@192.168.10.21>
2026-01-01 19:55:20 +0500

    updated xml

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -6,20 +6,8 @@ export default function sitemap(): MetadataRoute.Sitemap {
     {
       url: 'https://mreademz.vercel.app',
       lastModified: new Date(),
-      changeFrequency: 'yearly',
-      priority: 1,
-    },
-    {
-      url: 'https://mreademz.vercel.app',
-      lastModified: new Date(),
-      changeFrequency: 'monthly',
-      priority: 0.8,
-    },
-    {
-      url: 'https://mreademz.vercel.app',
-      lastModified: new Date(),
       changeFrequency: 'weekly',
-      priority: 0.5,
+      priority: 1,
     },
   ]
 }
