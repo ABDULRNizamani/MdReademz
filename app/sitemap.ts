5d8e1732f29ff589f586580e65e873185fe6149d
Abdul Rehman <abdulrehman@192.168.10.21>
2026-01-03 11:57:28 +0500

    upadted url

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -4,7 +4,7 @@ import { MetadataRoute } from 'next'
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
-      url: 'https://mreademz.vercel.app',
+      url: 'https://mdreademz.vercel.app',
       lastModified: new Date(),
       changeFrequency: 'weekly',
       priority: 1,
